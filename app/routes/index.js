115cc2a3e3b9f20c840b32705dad9db72970e023
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-11 11:50:24 -0700

    add ability for posting answers to questions, currently still on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      questions: this.store.findAll('question')
-      // answers: this.store.findAll('answer')
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
     });
+  },
+  actions: {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    }
   }
 });
